7ca838cfbe21c4a42f3bc52e3580b736917f8824
Haryo30 <hrydwntr22@gmail.com>
2023-10-20 20:07:35 +0700

    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const navbar = document.getElementsByTagName('nav')[0];
 window.addEventListener('scroll', function () {
-    console.log(this.window.scrollY)
+    // console.log(this.window.scrollY)
     if (window.scrollY > 1) {
         navbar.classList.replace('bg-transparant', 'nav-color');
     } else if (this.window.scrollY <= 0) {

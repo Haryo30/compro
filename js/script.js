127066ff9e34aa3b5b006b5ef7212da6c6481512
TopherCalvin <126538023+TopherCalvin@users.noreply.github.com>
2023-10-22 20:05:33 +0700

    modified:   css/responsive.css 	modified:   css/style.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,67 @@
-const navbar = document.getElementsByTagName('nav')[0];
-window.addEventListener('scroll', function () {
-    // console.log(this.window.scrollY)
-    if (window.scrollY > 1) {
-        navbar.classList.replace('bg-transparant', 'nav-color');
-    } else if (this.window.scrollY <= 0) {
-        navbar.classList.replace('nav-color', 'bg-transparant')
-    }
+// Navbar
+const navbar = document.getElementsByTagName("nav")[0];
+window.addEventListener("scroll", function () {
+  // console.log(this.window.scrollY)
+  if (window.scrollY > 1) {
+    navbar.classList.replace("bg-transparant", "nav-color");
+  } else if (this.window.scrollY <= 0) {
+    navbar.classList.replace("nav-color", "bg-transparant");
+  }
 });
 
-const cardLayanan = document.getElementById("card-layanan");
+// On Going
 const onGoing = document.getElementById("on-going");
 function openOnGoing() {
-    onGoing.style.display = "flex";
-    onGoing.style.transform = "scale(1)";
-    onGoing.style.visibility = "visible";
-    onGoing.style.opacity = "1";
+  onGoing.style.display = "flex";
+  onGoing.style.transform = "scale(1)";
+  onGoing.style.visibility = "visible";
+  onGoing.style.opacity = "1";
 }
-function closeOnGOing() {
-    onGoing.style.display = "none";
-    onGoing.style.transform = "scale(0)";
-    onGoing.style.visibility = "hidden";
-    onGoing.style.opacity = "0";
+function closeOnGoing() {
+  onGoing.style.display = "none";
+  onGoing.style.transform = "scale(0)";
+  onGoing.style.visibility = "hidden";
+  onGoing.style.opacity = "0";
+}
+
+// Slide-modal
+const propertySlider = document.getElementById("property-modal");
+function openSlider() {
+  propertySlider.style.display = "flex";
+  propertySlider.style.transform = "scale(1)";
+  propertySlider.style.visibility = "visible";
+  propertySlider.style.opacity = "1";
+}
+function closeSlider() {
+  propertySlider.style.display = "none";
+  propertySlider.style.transform = "scale(0)";
+  propertySlider.style.visibility = "hidden";
+  propertySlider.style.opacity = "0";
+}
+
+// Slide
+const img = [
+  "Assets/img/Fitur Rumah 1.png",
+  "Assets/img/Hero Image.png",
+  "Assets/img/Fitur Rumah 1.png",
+  "Assets/img/Hero Image.png",
+];
+let i = 0;
+const slide = document.getElementById("slide");
+slide.src = img[i];
+function changeImgRight() {
+  if (i < img.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+  slide.src = img[i];
+}
+function changeImgLeft() {
+  if (i == 0) {
+    i = img.length - 1;
+  } else {
+    i--;
+  }
+  slide.src = img[i];
 }
